94d45b33f74851dbd9cac34486ab39423d17f79d
Halil Utku Unlu <h.utkuunlu@gmail.com>
2019-07-28 15:03:22 +0400

    Autosuggestion installed

--- zshconfig.sh
+++ zshconfig.sh
@@ -3,6 +3,8 @@ function zshConfig {
     printf " ~/.zshrc will be replaced with link to template config\n"
     printf "Original zsh settings can be found in /home/$USER/.zshrc~ \n"	
 
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    
     ln -sb $PWD/templates/zshrc /home/$USER/.zshrc
     ln -sb $PWD/templates/aliases.zsh /home/$USER/.oh-my-zsh/custom/aliases.zsh
     
