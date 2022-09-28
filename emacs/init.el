16d53d51a0fd99b17e436fa2d740043a702814c4
Halil Utku Unlu <h.utkuunlu@gmail.com>
2022-09-27 19:03:13 -0500

    Initial keybindings for ivy-bibtex

--- emacs/init.el
+++ emacs/init.el
@@ -125,6 +125,10 @@
   (ivy-posframe-mode))
 
 (use-package ivy-bibtex
+  :bind
+  (:map global-map
+	("C-c i" . ivy-bibtex)
+	("C-c r" . ivy-resume))
   :config ;; Obtained from org-ref page
   (setq bibtex-completion-bibliography '("~/cloud/org/references/articles.bib")
 	bibtex-completion-library-path '("~/cloud/org/references/pdfs/")
