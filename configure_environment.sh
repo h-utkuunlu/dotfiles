ac9907e00860cea30b68f9afeb3450126282b575
Halil Utku Unlu <h.utkuunlu@gmail.com>
2022-12-01 20:19:01 +0400

    Basic qtile config

--- configure_environment.sh
+++ configure_environment.sh
@@ -94,6 +94,10 @@ ln -sb $PWD/emacs/early-init.el $HOME/.emacs.d/early-init.el
 emacs --daemon
 emacsclient -e "(kill-emacs)"
 
+##### qtile #####
+backup_file $HOME/.config/qtile/config.py
+ln -sb $PWD/qtile_config.py $HOME/.config/qtile/config.py 
+
 ##### clangd-format #####
 print_emph "** Configuring clang-format **"
 ln -sb $PWD/.clang-format $HOME/.clang-format
