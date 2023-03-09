47de38a213d4301b7f4258c05da425fe7b96aea4
Halil Utku Unlu <h.utkuunlu@gmail.com>
2023-03-09 13:57:50 +0400

    Discourage rm use

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,7 @@
 # Aliases
 alias open="xdg-open"
 alias sn="shutdown now"
+alias rm='echo "This is not the command you are looking for. Use trash-cli, or /bin/rm"; false'
 alias nyuadvpn="sudo openconnect --authgroup nyu-vpn vpn.abudhabi.nyu.edu"
 alias nyuvpn_all="sudo openconnect --authgroup 'NYU VPN: All Traffic' vpn.nyu.edu"
 alias nyuvpn_net="sudo openconnect --authgroup 'NYU VPN: NYU-Net Traffic Only' vpn.nyu.edu"
