8d854516ce08e9863ac58540f1aeeeb60133601a
Halil Utku Unlu <h.utkuunlu@gmail.com>
2023-03-24 13:29:03 +0400

    Aliasing with program existence checks & ls modifications

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,19 @@
 alias open="xdg-open"
 alias sn="shutdown now"
 alias rm='echo "This is not the command you are looking for. Use trash-cli, or /bin/rm"; false'
-alias nyuadvpn="sudo openconnect --authgroup nyu-vpn vpn.abudhabi.nyu.edu"
-alias nyuvpn_all="sudo openconnect --authgroup 'NYU VPN: All Traffic' vpn.nyu.edu"
-alias nyuvpn_net="sudo openconnect --authgroup 'NYU VPN: NYU-Net Traffic Only' vpn.nyu.edu"
+alias tp="trash-put"
+
+if command -v openconnect &> /dev/null
+then
+    alias nyuadvpn="sudo openconnect --authgroup nyu-vpn vpn.abudhabi.nyu.edu"
+    alias nyuvpn_all="sudo openconnect --authgroup 'NYU VPN: All Traffic' vpn.nyu.edu"
+    alias nyuvpn_net="sudo openconnect --authgroup 'NYU VPN: NYU-Net Traffic Only' vpn.nyu.edu"
+fi
+
+alias l="ls -lh"
+alias la="ls -lah"
+
+if command -v lsd &> /dev/null
+then 
+    alias ls="lsd"
+fi
